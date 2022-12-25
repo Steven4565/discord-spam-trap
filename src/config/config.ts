900e8013c3f63b54df7a660a734e508ed622eeea
NathanAdhitya <me@natha.my.id>
2022-12-25 16:34:37 +0700

    🎨 fix formatting for config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,47 +5,47 @@ import yaml from "js-yaml";
 const configPath = "config.yaml";
 
 export function loadConfig() {
-	let config: GuildListConfig = {} as GuildListConfig;
+  let config: GuildListConfig = {} as GuildListConfig;
 
-	try {
-		try {
-			fs.writeFileSync(configPath, "", { flag: "wx" });
-		} catch (e) {}
+  try {
+    try {
+      fs.writeFileSync(configPath, "", { flag: "wx" });
+    } catch (e) {}
 
-		config = (yaml.load(fs.readFileSync(configPath, "utf8")) ??
-			config) as GuildListConfig;
-	} catch (e) {
-		console.log(e);
-	}
-	return config;
+    config = (yaml.load(fs.readFileSync(configPath, "utf8")) ??
+      config) as GuildListConfig;
+  } catch (e) {
+    console.log(e);
+  }
+  return config;
 }
 
 export function updateConfig(config: GuildListConfig) {
-	try {
-		const configYaml = yaml.dump(config);
-		fs.writeFileSync(configPath, configYaml);
-	} catch (e) {
-		console.log(e);
-	}
+  try {
+    const configYaml = yaml.dump(config);
+    fs.writeFileSync(configPath, configYaml);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export function getGuildConfig(
-	config: GuildListConfig,
-	guildId: string
+  config: GuildListConfig,
+  guildId: string
 ): GuildConfig {
-	if (!config || !config[guildId]) {
-		const newConfig = newGuildConfig();
+  if (!config || !config[guildId]) {
+    const newConfig = newGuildConfig();
 
-		config[guildId] = newConfig;
-		return newConfig;
-	}
-	return config[guildId] as GuildConfig;
+    config[guildId] = newConfig;
+    return newConfig;
+  }
+  return config[guildId] as GuildConfig;
 }
 
 export function newGuildConfig(): GuildConfig {
-	return {
-		channelId: undefined,
-		enabled: true,
-		excluded: [],
-	};
+  return {
+    channelId: undefined,
+    enabled: true,
+    excluded: [],
+  };
 }
